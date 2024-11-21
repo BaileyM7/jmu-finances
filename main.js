d89fb2e4abae73210a1b29bde32529e35ab974c3
Bailey Malota <bmalota08@gmail.com>
2024-11-20 21:46:24 -0500

    adding all the nodes

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+/* 
+Collaborators:
+Graph 1:
+Graph 2: 
+Graph 3: 
+Graph 4: Bailey Malota
+*/ 
 import * as d3 from 'd3';
 import * as d3Sankey from "d3-sankey";
 
@@ -20,10 +27,150 @@ const sankey = d3Sankey.sankey()
   .nodeWidth(15)
   .nodePadding(10)
   .extent([[1, 5], [width - 1, height - 5]]);
+//================================= my work here ===================
+// gets the nodes and links for diagram 4
+function forDiagram4(JMUdata) {
+  const relevantData = JMUdata["jmu-athletics"];
+  // prints the raw form of the data
+  console.log(relevantData);
+
+  // getting the nodes for each column
+  const nodes1 = getNodes1(relevantData);
+  const nodes2 = getNodes2(relevantData);
+  const nodes3 = getNodes3(relevantData);
+  const nodes4 = getNodes4(relevantData);
+  const nodes5 = getNodes5(relevantData);
+
+
+  const links = getLinks(relevantData);
+  return { nodes, links };
+}
+
+// Gets the nodes for diagram 4 column 1
+function getNodes1(data) {
+  const result = {}; 
+
+  // iterates though every json node
+  for (const entry of data) {
+    if (entry.type === "Operating Revenues") {
+      // goes through every revenue
+      for (const key in entry) {
+        if (
+          key !== "name" &&
+          key !== "type" &&
+          key !== "Total"
+        ) {
+          // adds a new key if not in the dict
+          if (!result[key]) {
+            result[key] = { name: key, title: "Operating Revenues", total: 0 };
+          }
+          // if in dict already, att to its value
+          result[key].total += entry[key];
+        }
+      }
+    }
+  }
+  // turns the object into an array
+  console.log('column 1: ', result);
+  return Object.values(result);
+}
+
+// gets the nodes for diagram 4 column 2
+function getNodes2(data) {
+  const result = {}; 
+
+  // iterates though every json node
+  for (const entry of data) {
+    if (entry.type === "Operating Revenues") {
+      // goes through every revenue
+      result[entry.name] = { name: entry.name, title: entry.type, total: entry.Total };
+          
+    }
+  }
+  // turns the object into an array
+  console.log('column 2: ', result);
+  return Object.values(result);
+}
+
+// gets the nodes for giagram 4 columsn 3
+function getNodes3(data){
+  const result = {}; 
+  let sum = 0;
+  // iterates though every json node
+  for (const entry of data) {
+    if (entry.type === "Operating Revenues") {
+      // goes through every revenue
+      const value = Number(entry.Total);
+      sum += value;
+
+      result["JMU"] = { name: "JMU Revenue", title: entry.type, total: sum };
+    }
+  }
+  // turns the object into an array
+  console.log('column 3: ', result);
+  return Object.values(result);
+}
+
+// gets the nodes for giagram 4 columsn 5
+function getNodes4(data){
+  const result = {}; 
+
+  // iterates though every json node
+  for (const entry of data) {
+    if (entry.type === "Operating Expenses") {
+      // goes through every revenue
+      result[entry.name] = { name: entry.name, title: entry.type, total: entry.Total };
+          
+    }
+  }
+  // turns the object into an array
+  console.log('column 4: ', result);
+  return Object.values(result);
+}
+
+// gets the nodes for giagram 5 columsn 4
+function getNodes5(data){
+  const result = {}; 
+
+  // iterates though every json node
+  for (const entry of data) {
+    if (entry.type === "Operating Expenses") {
+      // goes through every revenue
+      for (const key in entry) {
+        if (
+          key !== "name" &&
+          key !== "type" &&
+          key !== "Total"
+        ) {
+          // adds a new key if not in the dict
+          if (!result[key]) {
+            result[key] = { name: key, title: "Operating Expenses", total: 0 };
+          }
+          // if in dict already, att to its value
+          result[key].total += entry[key];
+        }
+      }
+    }
+  }
+  // turns the object into an array
+  console.log('column 5: ', result);
+  return Object.values(result);
+}
+
+
+// gets the links for diagram 4
+function getLinks(relevantData) {
+
+}
+
+//================================= my work here ===================
 
 
 async function init() {
-  const data = await d3.json("data/data_sankey.json");
+  // const data = await d3.json("data/data_sankey.json");
+  const JMUdata = await d3.json("data/jmu.json");
+  const data = forDiagram4(JMUdata);
+
   // Applies it to the data. We make a copy of the nodes and links objects
   // so as to avoid mutating the original.
   const { nodes, links } = sankey({
@@ -124,6 +271,7 @@ async function init() {
 
   const svgNode = svg.node();
   document.body.appendChild(svgNode);
+
   return svgNode;
 }
 
